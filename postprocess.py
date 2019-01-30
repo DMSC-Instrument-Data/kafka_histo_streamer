8d5366e5ecbb3378465a4fca468adccab3b8f4d3
Michael Hart <michael.hart@stfc.ac.uk>
2019-01-30 14:35:48 +0000

    Added license file and headers

--- postprocess.py
+++ postprocess.py
@@ -1,3 +1,25 @@
+#  -*- coding: utf-8 -*-
+# *********************************************************************
+# Kafka Histogram Re-streamer
+# Copyright (C) 2019 European Spallation Source ERIC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Module authors:
+#   Michael Hart <michael.hart@stfc.ac.uk>
+# *********************************************************************
+
 # Special variables:
 #   input  = reference to input workspace (accumulation WS)
 #   output = name of the processed output workspace
